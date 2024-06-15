4d732bf3967490febbc6675baf0b5ec03e62cb63
LiangShaw <fti9r6@qq.com>
2024-06-15 13:51:45 +0800

    add violin plot for rmats

--- R/EasyParseDAS.R
+++ R/EasyParseDAS.R
@@ -12,8 +12,8 @@
 #' @param dec.color color for decreased point. Default 'blue'.
 #' @param ns.color color for not significantly changed point. Dafault 'grey60'.
 #'
-#' @import tidyverse dplyr
-#' @return a list containing table with change and volcano plot
+#' @import tidyverse dplyr ggsci ggprism
+#' @return a list containing table with change and volcano plot, violin plot
 #' @export
 #'
 #' @examples rmats_to_volcano_plot(tab,PValue.thres=1,FDR.thres=0.05,deltaPSI.thres=0.05,ylimit=c(0,10),xlimit=c(-1,1),volcano.title='Differentially AS (treat vs ctrl)',yaxis='FDR', inc.color='red',dec.color='blue',ns.color='grey60')
@@ -49,17 +49,15 @@ rmats_to_volcano_plot <- function(
   decrease.num <- modify.tab %>% filter(change=='dec') %>% nrow()
 
   event.count.tab <- modify.tab %>% select(EventType,change) %>% table() %>% as.data.frame() %>% 
-    mutate(x=ifelse(change=='dec', -xlimit[2]*0.75, xlimit[2]*0.75),
-           y=ylimit[2]*0.75 )
+    mutate(x=ifelse(change=='dec', -xlimit[2]*0.7, xlimit[2]*0.7),
+           y=ylimit[2]*0.7 )
 
   vol.plot <- modify.tab %>%
     ggplot(aes(x=IncLevelDifference ,y=yvalue, color=change)) +
     geom_point(size=0.1, alpha=0.1) +
+    geom_text(data=event.count.tab,mapping=aes(x=x,y=y,label=Freq),
+              size=7,size.unit='pt') + 
     theme_bw(base_size = 7) +
-    scale_color_manual(
-      values = c('inc'=inc.color,
-                 'dec'=dec.color,
-                 'n.s.'=ns.color)) +
     scale_x_continuous(limits = xlimit, expand = c(0,0)) +
     scale_y_continuous(limits = ylimit, expand = c(0,0)) +
     labs(x=paste0('deltaPSI\n(PValue:',PValue.thres,
@@ -69,14 +67,46 @@ rmats_to_volcano_plot <- function(
          title = volcano.title) +
     #annotate('text',x=-xlimit[2]*0.75,y=ylimit[2]*0.75, label=decrease.num) +
     #annotate('text',x=xlimit[2]*0.75,y=ylimit[2]*0.75, label=increase.num) + 
-    geom_text(data=event.count.tab,mapping=aes(x=x,y=y,label=Freq),
-              size=8,size.unit='pt') + 
     facet_grid(.~EventType) + 
     theme(
       panel.grid.minor = element_blank(),
       plot.title = element_text(hjust = 0.5, size=8),
       strip.background = element_blank()
-    ) 
+    ) + 
+    scale_color_manual(
+      values = c('inc'=inc.color,
+                 'dec'=dec.color,
+                 'n.s.'=ns.color))
 
-  return( list(modify.tab, vol.plot) )
+  violin.event.count.tab <- SBZ.HeLa.Cas9_vs_WT.volcano.list[[1]] %>% 
+  select(EventType,change) %>% 
+  table() %>% as.data.frame() %>% 
+  mutate(x=ifelse(change=='dec', -1,1),
+         y='1',
+         hjust=ifelse(change=='dec',0,1)
+         )
+
+  violin.plot <- modify.tab %>%
+    ggplot(aes(x=IncLevelDifference,y='1')) + 
+    geom_point(aes(color=EventType),size=0.3,alpha=0.3,
+              position = position_jitter(height = 0.1)) + 
+    #geom_boxplot(aes(group=change,fill=EventType),outliers = F,width=0.5,
+    #             position = position_dodge(width = 0,preserve = 'single')) + 
+    geom_violin(aes(group=change,fill=EventType),
+                position = position_dodge(width = 0)) +
+    geom_text(data = violin.event.count.tab,
+              mapping = aes(x=x,y=y,hjust=hjust,label=Freq),
+              size=7,size.unit='pt') +
+    scale_x_continuous(limits = c(-1,1),expand = c(0,0)) + 
+    scale_y_discrete(expand = c(0,0)) + 
+    theme_prism(base_size = 8) + 
+    theme(strip.background = element_blank(),
+          axis.text.y = element_blank(),
+          axis.ticks.y = element_blank(),
+          axis.line.y = element_blank()) + 
+    facet_grid(EventType~.) + 
+    scale_color_npg() + 
+    scale_fill_npg()
+
+  return( list(modify.tab, vol.plot, violin.plot) )
 }
