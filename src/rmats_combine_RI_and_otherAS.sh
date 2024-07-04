4101597e73eafda7734b4925c8c05937d93bb4d7
LiangShaw <fti9r6@qq.com>
2024-07-04 14:28:29 +0800

    correct rmats_combine_RI_and_otherAS.sh

--- src/rmats_combine_RI_and_otherAS.sh
+++ src/rmats_combine_RI_and_otherAS.sh
@@ -29,4 +29,4 @@ PVthres=$4
 FDRthres=$5
 PSIthres=$6
 
-awk '$9!="IR"' ${maintxt} | cat - ${RItxt} | awk  'NR==1 || $4<0.01 && $5<0.05 && ($8>=0.05 || $8<=-0.05)' > ${combine}.events.merge.maincol.PV${PVthres}_FDR${FDRthres}_dPSI${PSIthres}.txt
+awk '$9!="IR"' ${maintxt} | cat - ${RItxt} | awk -v PVthres=$PVthres -v FDRthres=$FDRthres -v dPSI=$PSIthres 'NR==1 || $8<PVthres && $9<FDRthres && ($12>=dPSI || $12<=-dPSI)' > ${combine}.events.merge.maincol.PV${PVthres}_FDR${FDRthres}_dPSI${PSIthres}.txt
